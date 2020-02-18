6ab2f060cf3ebf651802e4346b128e15222390f0
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-02-18 05:25:23 +0000

    Comment out last section on running using localhost:3000

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-// create an express object from the express package
+
 const express = require("express");
 const bodyParser = require("body-parser");
 
-// create an app object from the express object
+// create an app objndoe ect from the express object
 const app = express();
 // this allows the parsing of the html file using body parser
 app.use(bodyParser.urlencoded({extended: true}));
@@ -18,14 +18,15 @@ app.post("/", function(req, res){
   var radCircle = parseFloat(req.body.n1);;
 
 // does the computation of the input variables, as numbers
-  var areaCircle = Math.PI * radCircle * radCircle;
+    var areaCircle = Math.PI * Math.pow(radCircle, 2);
     var circumCircle = 2 * Math.PI * radCircle;
 
 // sends the results back to the web page as string
   res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2));
 })
 //this gets the response from the web page to this placeholder
+// COMMENT out this code in Repl, as this is not running on localhost:3000
 
-app.listen(3000, function() {
-  console.log ("Server is running on port 3000")
-});
+// app.listen(3000, function() {
+// console.log ("Server is running on port 3000")
+// });

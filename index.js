5e295633c19dcf8399831ba9b357f4ce2929304e
Debasis Bhattacharya <debasisb@hawaii.edu>
2020-02-18 03:03:18 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+// create an app object from the express object
+const app = express();
+// this allows the parsing of the html file using body parser
+app.use(bodyParser.urlencoded({extended: true}));
+
+//this sends the html file to the web page using the root directory
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html")
+});
+
+// this gets the response from the values in the web page
+app.post("/", function(req, res){
+
+  var radCircle = parseFloat(req.body.n1);;
+
+// does the computation of the input variables, as numbers
+  var areaCircle = Math.PI * radCircle * radCircle;
+    var circumCircle = 2 * Math.PI * radCircle;
+
+// sends the results back to the web page as string
+  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2));
+})
+//this gets the response from the web page to this placeholder
+
+app.listen(3000, function() {
+  console.log ("Server is running on port 3000")
+});

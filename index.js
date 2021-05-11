7b9c719bbf04cd69c40f91b99a346f8ffa2c6114
ckkeliih <>
2021-05-11 21:41:41 +0000

    Added volume

--- index.js
+++ index.js
@@ -15,14 +15,17 @@ app.get("/", function(req, res) {
 // this gets the response from the values in the web page
 app.post("/", function(req, res){
 
-  var radCircle = parseFloat(req.body.n1);;
+  var radCircle = parseFloat(req.body.n1);
+  var htCircle = parseFloat(req.body.n2);
+  
 
 // does the computation of the input variables, as numbers
     var areaCircle = Math.PI * Math.pow(radCircle, 2);
     var circumCircle = 2 * Math.PI * radCircle;
+    var volCircle = Math.PI * (Math.pow(radCircle,2) * htCircle);
 
 // sends the results back to the web page as string
-  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2));
+  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2) + " and the Volume is " + volCircle.toFixed(2));
 })
 //this gets the response from the web page to this placeholder
 // COMMENT out this code in Repl, as this is not running on localhost:3000

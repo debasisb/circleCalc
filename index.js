7d11089a2f2e98c7607869fe320f82e0d405fdeb
Jeffrey Wayer <78135433+JWayer@users.noreply.github.com>
2022-05-11 00:28:22 +0000

    Added another input to the screen for height of cylinder

--- index.js
+++ index.js
@@ -16,13 +16,17 @@ app.get("/", function(req, res) {
 app.post("/", function(req, res){
 
   var radCircle = parseFloat(req.body.n1);;
+  
+  var heightCyl = parseFloat(req.body.n2);;
 
 // does the computation of the input variables, as numbers
     var areaCircle = Math.PI * Math.pow(radCircle, 2);
     var circumCircle = 2 * Math.PI * radCircle;
+  
+  let volCircle = Math.PI * (Math.pow(radCircle, 2)) * heightCyl
 
 // sends the results back to the web page as string
-  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2));
+  res.send("The Area of the Cirle is " + areaCircle.toFixed(2) + " and the Circumference is " + circumCircle.toFixed(2) + " and the Volume is" + volCircle.toFixed(2));
 })
 //this gets the response from the web page to this placeholder
 // COMMENT out this code in Repl, as this is not running on localhost:3000
